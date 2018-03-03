395c0be527b5ae25d5772ac49e5d8fcb40db6996
Ahmed Fasih <wuzzyview@gmail.com>
2018-03-02 23:35:16 -0500

    Tests using tape

---
+++ tests/test.js
@@ -0,0 +1,36 @@
+const test = require('tape');                     // Tests
+const getRandomPort = require('get-random-port'); // A reliable way to get unused ports
+const fs = require('fs');                         // We'll slurp the saved CSV file from disk
+const http = require('http');                     // We'll serve the CSV from a temporary webserver
+const wiki = require('../index');                 // The module under test
+
+// Slurp the file from disk. `__dirname` because the CSV will be in the same directory as this test.
+const index = fs.readFileSync(__dirname + '/wikipedias-2018-03-02.csv', 'utf8');
+
+test('Loading and parsing cached Wikimedia Foundation CSV data', async (t) => {
+  const port = await getRandomPort();
+  // Create an HTTP server that'll just dump the CSV data for each request
+  let server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type' : 'text/plain' });
+    res.end(index);
+  });
+
+  // Once the HTTP server is up and running, then begin our tests
+  server.listen(port, async () => {
+    // Invoke the module under test
+    let data = await wiki(`http://localhost:${port}`, 'lang;prefix;edits;loclang'.split(';'));
+
+    t.ok(data.every(o => o.hasOwnProperty('lang') && o.hasOwnProperty('prefix') && o.hasOwnProperty('edits')
+                 && o.hasOwnProperty('loclang')),
+        'Each row has the fields we want');
+
+    t.ok(data.every(o => typeof o.prefix === 'string'), 'Each row has stringy `prefix`');
+    t.ok(data.every(o => typeof o.edits === 'number'), 'Each row has numeric `edits`');
+    t.ok(data.some(o => o.lang === 'French' && o.loclang === 'Français'), 'HTML escapes unescaped, i.e., Français');
+
+    // All done testing. Close the webserver (so this script can end) and tell tape we're done.
+    server.close();
+    t.end();
+  });
+
+})
